f4df78eb034a84b8f0274d115d4bc8d613a79d2a
Sushil <sushilt553@gmail.com>
2020-07-02 13:58:27 -0400

    Pokemon detail component

---
+++ frontend/components/pokemon/pokemon_detail.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class PokemonDetail extends React.Component {
+
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default PokemonDetail;
