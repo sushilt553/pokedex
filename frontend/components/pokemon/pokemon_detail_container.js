d913a53d1368c25e60f774792e14a93d5066bb9e
Sushil <sushilt553@gmail.com>
2020-07-02 13:59:52 -0400

    pokemon detail container

---
+++ frontend/components/pokemon/pokemon_detail_container.js
@@ -0,0 +1,16 @@
+import {connect} from 'react-redux';
+import PokemonDetail from './pokemon_detail';
+
+const mapStateToProps = state => {
+    return {
+
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonDetail);
