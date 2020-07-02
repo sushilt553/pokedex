0ed7c9c390f0ffba61b34b2bb59000c983de33c3
Sushil <sushilt553@gmail.com>
2020-07-02 14:02:06 -0400

    API util for pokemon show

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import {Route, HashRouter} from 'react-router-dom';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
+import PokemonDetailContainer from './pokemon/pokemon_detail_container';
 
 const Root = ({ store }) => (
     <Provider store={store}>
         <HashRouter>
-            <Route path="/pokemon/:pokemonId" component={}></Route>
+            <Route path="/pokemon/:pokemonId" component={PokemonDetailContainer}></Route>
             <Route path="/" component={PokemonIndexContainer}></Route>
         </HashRouter>
     </Provider>
