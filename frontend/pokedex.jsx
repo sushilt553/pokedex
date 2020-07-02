16638e584f7c94bc17cdc95364d6dbf1657d8fa8
Sushil <sushilt553@gmail.com>
2020-07-02 13:27:58 -0400

    store test

--- frontend/pokedex.jsx
+++ frontend/pokedex.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {receiveAllPokemon} from './actions/pokemon_actions';
 import {fetchAllPokemon} from './util/api_util';
+import configureStore from './store/store';
 
+//for test purpose only
 window.receiveAllPokemon = receiveAllPokemon;
 window.fetchAllPokemon = fetchAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+    //for test purpose only
+    window.store = store;
+    window.dispatch = store.dispatch;
     ReactDOM.render(<h1>React is working</h1>, root);
 })
