864296582fe2f176034a6dceab53d0fade2d4ff9
Sushil <sushilt553@gmail.com>
2020-07-02 13:12:00 -0400

    React connected to rails

---
+++ app/controllers/api/pokemon_controller.rb
@@ -0,0 +1,20 @@
+class Api::PokemonController < ApplicationController
+
+    def index
+        @pokemon = Pokemon.all
+        render :index
+    end
+
+    def show
+        @pokemon = Pokemon.find_by(id: params[:id])
+        render :show
+    end
+
+    def create
+    end
+
+    private
+    def pokemon_params
+        params.require(:pokemon).permit(:name, :attack, :defense, :poke_type, :moves, :image_url)
+    end
+end
