c35904f921e881cda8ff39270e427ba3bcbcd9bc
Sushil <sushilt553@gmail.com>
2020-07-02 13:51:21 -0400

    Hash router and route library

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import {Route, HashRouter} from 'react-router-dom';
 import PokemonIndexContainer from './pokemon/pokemon_index_container';
 
 const Root = ({ store }) => (
     <Provider store={store}>
-        <PokemonIndexContainer />
+        <HashRouter>
+            <PokemonIndexContainer />
+        </HashRouter>
     </Provider>
 );
 
